ebb13f4320aeb7b141ff9d72eb5a7c167964be17
Davide <galluzzodavide01@gmail.com>
2025-07-06 11:39:40 +0200

    map.js

--- OnePage/assets/js/map.js
+++ OnePage/assets/js/map.js
@@ -1,20 +1,17 @@
 
-import 'https://cdn.jsdelivr.net/npm/ol@v10.5.0/dist/ol.js';
-import LayerSwitcher from 'https://cdn.jsdelivr.net/npm/ol-layerswitcher@4.1.1/dist/ol-layerswitcher.esm.js';
-import 'ol/ol.css';
-import 'ol-layerswitcher/dist/ol-layerswitcher.css';
 import { Map, View, Overlay } from 'ol';
 import { Tile, Image, Group, Vector } from 'ol/layer';
 import { OSM, ImageWMS, XYZ, StadiaMaps } from 'ol/source';
 import VectorSource from 'ol/source/Vector';
 import { GeoJSON } from 'ol/format';
 import { fromLonLat } from 'ol/proj';
-import { ScaleLine, FullScreen, MousePosition, } from 'ol/control';
+import { ScaleLine, FullScreen, MousePosition } from 'ol/control';
 import LayerSwitcher from 'ol-layerswitcher';
 import { createStringXY } from 'ol/coordinate';
 import { Style, Fill, Stroke } from 'ol/style';
 import Map from 'ol/Map';
 import View from 'ol/View';
+
 
 // MAPPA BASE
 const osm = new ol.layer.Tile({
